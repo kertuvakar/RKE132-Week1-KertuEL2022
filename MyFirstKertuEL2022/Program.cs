2a1fea68b7e38abbf3ea09f4dc18ec039e9eb940
Kertu Vakar <kertu.vakar@tktk.ee>
2024-02-01 08:55:53 +0200

    create new app

---
+++ MyFirstKertuEL2022/Program.cs
@@ -0,0 +1,19 @@
+﻿// This is a comment 
+//Rakendus küsib kasutajalt sisesta oma nime ja seejärel rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:");
+//String on sõne. Panen arvuti mälusse kastikesse,kuhu salvestan andmetüübi sõne
+//Muid andmetüüpe sinna kirjutada ei saa
+String userName = Console.ReadLine();
+//võrdusmark tähendab, et salvesta väärtus
+//järgmine rida loeb sisu konsoolist mällu salvestab selle kasti userName
+
+//järgmisena rakendus tervitab nimepidi
+//ka jutumärkides olev asi on sõne
+//Pluss lihtsalt liidab andmeväljad kokku, see ei tekita tühikuid konsoolis
+//Console.WriteLine("Hello" + ", " + userName + "!") aga see on liiga pikk variant
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+//Teksti peab piirama hoidma jutumärkide vahel
+//votan andmed mida olen kasutanud ja lisan need uuele koodile
+Console.WriteLine($"Hello, {userName}!");
